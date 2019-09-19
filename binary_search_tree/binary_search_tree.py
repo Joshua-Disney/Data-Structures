7262df204513254b1ab0e44e77ed8850ac87d698
joshua-disney <joshua.a.disney@gmail.com>
2019-09-19 13:05:35 -0700

    Bring in new functions, tests, and files

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,5 @@
-# from dll_stack import Stack
-# from dll_queue import Queue
-# import sys
-# sys.path.append('../queue_and_stack')
+from dll_stack import Stack
+from dll_queue import Queue
 
 
 class BinarySearchTree:
@@ -73,3 +71,38 @@ class BinarySearchTree:
                 loop_queue.insert(0, node.left)
             if node.right is not None:
                 loop_queue.insert(0, node.right)
+
+    def forEach(self, cb):
+        cb(self.value)
+        if self.left:
+            self.left.forEach(cb)
+        if self.right:
+            self.right.forEach(cb)
+# DAY 2 Project -----------------------
+
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
+
+    def in_order_dft(self, node):
+        pass
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative breadth first traversal
+    def bft_print(self, node):
+        pass
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative depth first traversal
+    def dft_print(self, node):
+        pass
+
+    # STRETCH Goals -------------------------
+    # Note: Research may be required
+
+    # Print In-order recursive DFT
+    def pre_order_dft(self, node):
+        pass
+
+    # Print Post-order recursive DFT
+    def post_order_dft(self, node):
+        pass
---
+++ binary_search_tree/dll_stack.py
@@ -0,0 +1,19 @@
+from doubly_linked_list import DoublyLinkedList
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = DoublyLinkedList()
+
+    def push(self, value):
+        self.storage.add_to_head(value)
+        self.size += 1
+
+    def pop(self):
+        self.size -= 1
+        self.storage.remove_from_head()
+
+    def len(self):
+        return self.size
